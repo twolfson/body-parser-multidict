f04b5a7cc5768a104959a32eda8867de6f87d4f0
Todd Wolfson <todd@twolfson.com>
2016-06-07 11:35:25 +0200

    Fixed up multidict issues

--- test/urlencoded.js
+++ test/urlencoded.js
@@ -16,7 +16,7 @@ describe('bodyParser.urlencoded()', function(){
     .post('/')
     .set('Content-Type', 'application/x-www-form-urlencoded')
     .send('user=tobi')
-    .expect(200, '{"user":"tobi"}', done)
+    .expect(200, '{"user":["tobi"]}', done)
   })
 
   it('should 400 when invalid content-length', function(done){
@@ -65,141 +65,7 @@ describe('bodyParser.urlencoded()', function(){
     .post('/')
     .set('Content-Type', 'application/x-www-form-urlencoded')
     .send('user=tobi')
-    .expect(200, '{"user":"tobi"}', done)
-  })
-
-  it('should parse extended syntax', function(done){
-    request(server)
-    .post('/')
-    .set('Content-Type', 'application/x-www-form-urlencoded')
-    .send('user[name][first]=Tobi')
-    .expect(200, '{"user":{"name":{"first":"Tobi"}}}', done)
-  })
-
-  describe('with extended option', function(){
-    describe('when false', function(){
-      var server;
-      before(function(){
-        server = createServer({ extended: false })
-      })
-
-      it('should not parse extended syntax', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user[name][first]=Tobi')
-        .expect(200, '{"user[name][first]":"Tobi"}', done)
-      })
-
-      it('should parse multiple key instances', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user=Tobi&user=Loki')
-        .expect(200, '{"user":["Tobi","Loki"]}', done)
-      })
-    })
-
-    describe('when true', function(){
-      var server;
-      before(function(){
-        server = createServer({ extended: true })
-      })
-
-      it('should parse multiple key instances', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user=Tobi&user=Loki')
-        .expect(200, '{"user":["Tobi","Loki"]}', done)
-      })
-
-      it('should parse extended syntax', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user[name][first]=Tobi')
-        .expect(200, '{"user":{"name":{"first":"Tobi"}}}', done)
-      })
-
-      it('should parse parameters with dots', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user.name=Tobi')
-        .expect(200, '{"user.name":"Tobi"}', done)
-      })
-
-      it('should parse fully-encoded extended syntax', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('user%5Bname%5D%5Bfirst%5D=Tobi')
-        .expect(200, '{"user":{"name":{"first":"Tobi"}}}', done)
-      })
-
-      it('should parse array index notation', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('foo[0]=bar&foo[1]=baz')
-        .expect(200, '{"foo":["bar","baz"]}', done)
-      })
-
-      it('should parse array index notation with large array', function(done){
-        var str = 'f[0]=0'
-
-        for (var i = 1; i < 500; i++) {
-          str += '&f[' + i + ']=' + i.toString(16)
-        }
-
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send(str)
-        .expect(function (res) {
-          var obj = JSON.parse(res.text)
-          assert.equal(Object.keys(obj).length, 1)
-          assert.equal(Array.isArray(obj.f), true)
-          assert.equal(obj.f.length, 500)
-        })
-        .expect(200, done)
-      })
-
-      it('should parse array of objects syntax', function(done){
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send('foo[0][bar]=baz&foo[0][fizz]=buzz')
-        .expect(200, '{"foo":[{"bar":"baz","fizz":"buzz"}]}', done)
-      })
-
-      it('should parse deep object', function (done) {
-        var str = 'foo'
-
-        for (var i = 0; i < 500; i++) {
-          str += '[p]'
-        }
-
-        str += '=bar'
-
-        request(server)
-        .post('/')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send(str)
-        .expect(function (res) {
-          var obj = JSON.parse(res.text)
-          assert.equal(Object.keys(obj).length, 1)
-          assert.equal(typeof obj.foo, 'object')
-
-          var depth = 0
-          var ref = obj.foo
-          while (ref = ref.p) { depth++ }
-          assert.equal(depth, 500)
-        })
-        .expect(200, done)
-      })
-    })
+    .expect(200, '{"user":["tobi"]}', done)
   })
 
   describe('with inflate option', function(){
@@ -229,7 +95,7 @@ describe('bodyParser.urlencoded()', function(){
         test.set('Content-Encoding', 'gzip')
         test.set('Content-Type', 'application/x-www-form-urlencoded')
         test.write(new Buffer('1f8b080000000000000bcb4bcc4db57db16e170099a4bad608000000', 'hex'))
-        test.expect(200, '{"name":"论"}', done)
+        test.expect(200, '{"name":["论"]}', done)
       })
     })
   })
@@ -427,7 +293,7 @@ describe('bodyParser.urlencoded()', function(){
         .post('/')
         .set('Content-Type', 'application/vnd.x-www-form-urlencoded')
         .send('user=tobi')
-        .expect(200, '{"user":"tobi"}', done)
+        .expect(200, '{"user":["tobi"]}', done)
       })
 
       it('should ignore standard type', function (done) {
@@ -451,7 +317,7 @@ describe('bodyParser.urlencoded()', function(){
         .post('/')
         .set('Content-Type', 'application/vnd.something')
         .send('user=tobi')
-        .expect(200, '{"user":"tobi"}', done)
+        .expect(200, '{"user":["tobi"]}', done)
       })
 
       it('should work without content-type', function (done) {
@@ -463,7 +329,7 @@ describe('bodyParser.urlencoded()', function(){
 
         var test = request(server).post('/')
         test.write('user=tobi')
-        test.expect(200, '{"user":"tobi"}', done)
+        test.expect(200, '{"user":["tobi"]}', done)
       })
 
       it('should not invoke without a body', function (done) {
@@ -530,7 +396,7 @@ describe('bodyParser.urlencoded()', function(){
       .post('/')
       .set('Content-Type', 'application/x-www-form-urlencoded')
       .send('user=tobi')
-      .expect(200, '{"user":"tobi"}', done)
+      .expect(200, '{"user":["tobi"]}', done)
     })
 
     it('should 415 on unknown charset prior to verify', function (done) {
@@ -555,7 +421,7 @@ describe('bodyParser.urlencoded()', function(){
       var test = request(server).post('/')
       test.set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
       test.write(new Buffer('6e616d653de8aeba', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should parse when content-length != char length', function(done){
@@ -563,14 +429,14 @@ describe('bodyParser.urlencoded()', function(){
       test.set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
       test.set('Content-Length', '7')
       test.write(new Buffer('746573743dc3a5', 'hex'))
-      test.expect(200, '{"test":"å"}', done)
+      test.expect(200, '{"test":["å"]}', done)
     })
 
     it('should default to utf-8', function(done){
       var test = request(server).post('/')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('6e616d653de8aeba', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should fail on unknown charset', function(done){
@@ -591,7 +457,7 @@ describe('bodyParser.urlencoded()', function(){
       var test = request(server).post('/')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('6e616d653de8aeba', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should support identity encoding', function(done){
@@ -599,7 +465,7 @@ describe('bodyParser.urlencoded()', function(){
       test.set('Content-Encoding', 'identity')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('6e616d653de8aeba', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should support gzip encoding', function(done){
@@ -607,7 +473,7 @@ describe('bodyParser.urlencoded()', function(){
       test.set('Content-Encoding', 'gzip')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('1f8b080000000000000bcb4bcc4db57db16e170099a4bad608000000', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should support deflate encoding', function(done){
@@ -615,7 +481,7 @@ describe('bodyParser.urlencoded()', function(){
       test.set('Content-Encoding', 'deflate')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('789ccb4bcc4db57db16e17001068042f', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should be case-insensitive', function(done){
@@ -623,7 +489,7 @@ describe('bodyParser.urlencoded()', function(){
       test.set('Content-Encoding', 'GZIP')
       test.set('Content-Type', 'application/x-www-form-urlencoded')
       test.write(new Buffer('1f8b080000000000000bcb4bcc4db57db16e170099a4bad608000000', 'hex'))
-      test.expect(200, '{"name":"论"}', done)
+      test.expect(200, '{"name":["论"]}', done)
     })
 
     it('should fail on unknown encoding', function(done){
@@ -661,7 +527,7 @@ function createServer(opts){
   return http.createServer(function(req, res){
     _bodyParser(req, res, function(err){
       res.statusCode = err ? (err.status || 500) : 200;
-      res.end(err ? err.message : JSON.stringify(req.body));
+      res.end(err ? err.message : JSON.stringify(req.body.toObject()));
     })
   })
 }
